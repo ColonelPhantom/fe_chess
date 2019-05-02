1e7b6e4554b5681aeb08508f6525bc1855be359f
Quinten Kock <quinten@quinten.space>
2019-05-02 13:29:01 +0200

    Start to implement board logic

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,6 @@
-enum PieceType {
+#[derive(Copy, Clone)]
+#[repr(u8)]
+pub enum PieceType {
     None = 0,       // if(Piece)
     WPawn = 1,      // all odd can move diagonally
     WKnight = 2,    // &2 is minor piece
@@ -16,7 +18,42 @@ enum PieceType {
     BAny = 15
 }
 
-struct Board {
+type Coord88 = u8;
+
+struct Unmake {
+    from: Coord88,
+    tp: Coord88,
+    captured: PieceType
+}
+
+pub struct Board {
     // Mailbox: 0x88
-    mailbox: [PieceType; 128]
+    mailbox: [PieceType; 128],
+    bitboards: [u64; 16],
+    unmake_stack: Vec<Unmake>
+
+}
+
+impl Board {
+    pub fn new_board_startpos() -> Board {
+        //use PieceType::{None, WPawn, WKnight, WBishop, WRook, WQueen, WKing, BPawn, BKnight, BBishop, BRook, BQueen, BKing};
+        use PieceType::*;
+        Board {
+            mailbox: [
+                WRook,  WKnight,    WBishop,    WQueen, WKing,  WBishop,    WKnight,    WRook,  None,   None,   None,   None,   None,   None,   None,   None,   
+                WPawn,  WPawn,      WPawn,      WPawn,  WPawn,  WPawn,      WPawn,      WPawn,  None,   None,   None,   None,   None,   None,   None,   None,   
+                None,   None,       None,       None,   None,   None,       None,       None,   None,   None,   None,   None,   None,   None,   None,   None,             
+                None,   None,       None,       None,   None,   None,       None,       None,   None,   None,   None,   None,   None,   None,   None,   None,             
+                None,   None,       None,       None,   None,   None,       None,       None,   None,   None,   None,   None,   None,   None,   None,   None,             
+                None,   None,       None,       None,   None,   None,       None,       None,   None,   None,   None,   None,   None,   None,   None,   None,             
+                BPawn,  BPawn,      BPawn,      BPawn,  BPawn,  BPawn,      BPawn,      BPawn,  None,   None,   None,   None,   None,   None,   None,   None,   
+                BRook,  BKnight,    BBishop,    BQueen, BKing,  BBishop,    BKnight,    BRook,  None,   None,   None,   None,   None,   None,   None,   None,   
+
+            ],
+
+            bitboards: [0; 16],  // Use empty bitboards for now (yes that's gross)
+
+            unmake_stack: Vec::new()
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ mod board;
 
 fn main() {
     println!("Hello, world!");
+
+    let b = board::Board::new_board_startpos();
 }
