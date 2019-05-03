18a38f8f1bf419b0e7d29bbcf42e9ccd54b2d33b
Quinten Kock <quinten@quinten.space>
2019-05-03 21:45:57 +0200

    Make c0x88.lua less ugly

--- src/board/c0x88.lua
+++ src/board/c0x88.lua
@@ -1,17 +1,13 @@
 for rank=1,8 do
     for file=1,8 do
+        sq0x88 = (16 * (rank-1)) + (file-1);
     
-    
-    sq0x88 = (16 * (rank-1)) + (file-1);
-    
-    print(
-    "pub const "..
-    string.char(file-1 + string.byte("a"))..rank..
-    ": Coord0x88 = Wrapping(0x"..
-    string.format("%02x", sq0x88)
-    ..");"
-    )
-    
-    
+        print(
+            "pub const "..
+            string.char(file-1 + string.byte("a"))..rank..
+            ": Coord0x88 = Wrapping(0x"..
+            string.format("%02x", sq0x88)
+            ..");"
+        )
     end
-    end
+end
