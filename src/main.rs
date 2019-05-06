74d58d526edeebc24e8bb093dc55e94b4a278c31
Quinten Kock <quintenkock@gmail.com>
2019-05-06 16:53:45 +0200

    Add board printing and change perft tests again

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,31 @@ fn pretty_perft(b: &mut board::Board, depth: usize) -> usize {
     return perft_count;
 }
 
+fn print_board(b: &board::Board) {
+    for rank in (0..8).rev() {
+        for file in 0..8 {
+            use board::PieceType::*;
+            let p = b[board::c0x88(file, rank)];
+            let mut t = match p.piece_type {
+                Pawn => 'p',
+                Knight => 'n',
+                Bishop => 'b',
+                Rook => 'r',
+                Queen => 'q',
+                King => 'k',
+                _ => ' ',
+            };
+            match p.color {
+                board::WHITE => { t.make_ascii_lowercase(); }
+                board::BLACK => { t.make_ascii_uppercase(); }
+            }
+            print!("{}", t);
+        }
+        println!("");
+    }
+    println!("Kingpos: {:02x}, {:02x}", b.king_pos[board::WHITE as usize], b.king_pos[board::BLACK as usize]);
+}
+
 fn main() {
     let mut b = board::Board::new();
 
@@ -62,26 +87,14 @@ fn main() {
 
     //println!("\n\n");
     println!("Perft test, depth 1: {}", perft(&mut b, 1));
-
     b = board::Board::new();
     println!("Perft test, depth 2: {}", perft(&mut b, 2));
-    //println!("Perft test, depth 2: {}", perft(&mut b, 1));
-    // let moves = movegen::movegen(&b);
-    // for m in moves {
-    //     println!("{:02x} to {:02x}, promote: {:?}, enpassant: {:?}", m.from, m.to, m.promote_to, m.en_passant);
-    // }
-
-    //for i in 1..32 {
-    //    println!("{:02x}", i);
-    //}
-
     b = board::Board::new();
     println!("Perft test, depth 3: {}", perft(&mut b, 3));
     b = board::Board::new();
-    println!("Perft test, depth 4: {}", pretty_perft(&mut b, 4));
-
+    println!("Perft test, depth 4: {}", perft(&mut b, 4));
     b = board::Board::new();
-    println!("Perft test, depth 5: {}", perft(&mut b, 5));
+    println!("Perft test, depth 5: {}", pretty_perft(&mut b, 5));
     b = board::Board::new();
     println!("Perft test, depth 6: {}", perft(&mut b, 6));
 
