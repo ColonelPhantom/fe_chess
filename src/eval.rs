1ae29699894331219cd93ace9d3cb45a1de8a83d
Quinten Kock <quinten@quinten.space>
2019-05-09 00:12:48 +0200

    eval() skeleton in place, eval constants naming

--- src/eval.rs
+++ src/eval.rs
@@ -2,10 +2,14 @@ use crate::board::*;
 
 type ValCp = usize;
 
-const king_val: ValCp = 200000;
-const queen_val: ValCp = 900;
-const rook_val: ValCp = 500;
-const bishop_val: ValCp = 320;
-const knight_val: ValCp = 330;
+const KING_VAL: ValCp = 200000;
+const QUEEN_VAL: ValCp = 900;
+const ROOK_VAL: ValCp = 500;
+const BISHOP_VAL: ValCp = 320;
+const KNIGHT_VAL: ValCp = 330;
 
 const BISHOP_PAIR_BONUS: ValCp = 25;
+
+pub fn eval(b: &Board) -> isize {
+    return 0;
+}
