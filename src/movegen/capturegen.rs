7b19bb6641be8fa92e46450a3dabda0135b101ce
Quinten Kock <quinten@quinten.space>
2019-05-09 01:41:32 +0200

    Add a capture generator

--- src/movegen/capturegen.rs
+++ src/movegen/capturegen.rs
@@ -1,9 +1,7 @@
 use crate::board;
 use board::*;
 
-mod capturegen;
-
-pub fn movegen(b: &mut Board) -> Vec<Move> {
+pub fn cap_gen(b: &Board) -> Vec<Move> {
     let mut moves: Vec<Move> = Vec::with_capacity(218);
     for rank in 0..8 { for file in 0..8 { 
         let c: Coord0x88 = c0x88(file,rank);
@@ -11,7 +9,7 @@ pub fn movegen(b: &mut Board) -> Vec<Move> {
         if p.color != b.side_to_move { continue; }   // Piece not of side to move: cannot move
 
         macro_rules! nonslide_move { ($to:expr) => {
-            if $to.0 & 0x88 == 0 && (!b.occupied($to) || b[$to].color != p.color ) {
+            if $to.0 & 0x88 == 0 && b.occupied($to) && b[$to].color != p.color ) {
                 moves.push(Move::new(c, $to));
             }
         };}
@@ -25,8 +23,6 @@ pub fn movegen(b: &mut Board) -> Vec<Move> {
                             moves.push(Move::new(c, to));
                         }
                         break;
-                    } else {
-                        moves.push(Move::new(c, to));
                     }
                     to += $offset;
                 }
@@ -48,23 +44,6 @@ pub fn movegen(b: &mut Board) -> Vec<Move> {
                 }
                 match p.color {
                     WHITE => {
-                        // Move ahead
-                        if !b.occupied(c + o0x88(0,1)) {
-                            if rank == 6 {  // 6 is second last
-                                gen_promotions!(c, c+o0x88(0, 1));
-                            } else {
-                                moves.push( Move::new( c, c+o0x88(0,1) ) );
-                                if rank == 1 && !b.occupied( c + o0x88(0,2)) {
-                                    // Pawn first move, two ahead
-                                    moves.push( Move {
-                                        from: c, to: c+o0x88(0,2),
-                                        promote_to: PieceType::None,
-                                        en_passant: EnPassantState::Possible( c+o0x88(0,2) ),
-                                        castling: None,
-                                    });
-                                }
-                            }
-                        }
                         // En passant capture
                         if
                                 b.en_passant.is_some() &&
@@ -98,23 +77,6 @@ pub fn movegen(b: &mut Board) -> Vec<Move> {
                         }
                     },
                     BLACK => {
-                        // Move ahead
-                        if !b.occupied(c + o0x88(0,-1)) {
-                            if rank == 7-6 {  // 6 is second last
-                                gen_promotions!(c, c+o0x88(0, -1));
-                            } else {
-                                moves.push( Move::new( c, c+o0x88(0,-1) ) );
-                                if rank == 7-1 && !b.occupied( c + o0x88(0,-2)) {
-                                    // Pawn first move, two ahead
-                                    moves.push( Move {
-                                        from: c, to: c+o0x88(0,-2),
-                                        promote_to: PieceType::None,
-                                        en_passant: EnPassantState::Possible( c+o0x88(0,-2) ),
-                                        castling: None,
-                                    });
-                                }
-                            }
-                        }
                         // En passant capture
                         if
                                 b.en_passant.is_some() &&
@@ -191,45 +153,6 @@ pub fn movegen(b: &mut Board) -> Vec<Move> {
                 nonslide_move!(c+o0x88(-1,  0));
                 nonslide_move!(c+o0x88( 0,  1));
                 nonslide_move!(c+o0x88( 0, -1));
-
-                // Castling
-                let kc = b.king_pos[b.side_to_move as usize];
-                if 
-                        b.castling[CR_KING + b.side_to_move as usize] &&
-                        !b.occupied(kc + o0x88(1, 0)) &&
-                        !b.occupied(kc + o0x88(2, 0)) &&
-                        b.is_check(b.side_to_move) == ThreatInfo::Safe &&
-                        b.under_attack(kc + o0x88(1, 0), b.side_to_move) == ThreatInfo::Safe &&
-                        b.under_attack(kc + o0x88(2, 0), b.side_to_move) == ThreatInfo::Safe
-                {
-                    moves.push( Move {
-                        from: c,
-                        to: c + o0x88(2, 0),
-                        promote_to: PieceType::None,
-                        en_passant: EnPassantState::None,
-                        castling: Some(CR_KING),
-                    });
-                }
-
-                if 
-                        b.castling[CR_QUEEN + b.side_to_move as usize] &&
-                        !b.occupied(kc + o0x88(-1, 0)) &&
-                        !b.occupied(kc + o0x88(-2, 0)) &&
-                        !b.occupied(kc + o0x88(-3, 0)) &&
-                        b.is_check(b.side_to_move) == ThreatInfo::Safe &&
-                        b.under_attack(kc + o0x88(-1, 0), b.side_to_move) == ThreatInfo::Safe &&
-                        b.under_attack(kc + o0x88(-2, 0), b.side_to_move) == ThreatInfo::Safe
-                {
-                    moves.push( Move {
-                        from: c,
-                        to: c + o0x88(-2, 0),
-                        promote_to: PieceType::None,
-                        en_passant: EnPassantState::None,
-                        castling: Some(CR_QUEEN),
-                    });
-                }
-
-
             }
         }
     }}
