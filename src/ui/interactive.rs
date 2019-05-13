589c9c8c8b80c83a8030b7364e50af1f52756c39
Quinten Kock <quinten@quinten.space>
2019-05-13 03:30:32 +0200

    Add other 'UIs' in interactive (shrust)

--- src/ui/interactive.rs
+++ src/ui/interactive.rs
@@ -1,3 +1,20 @@
+use std::io::prelude::*;
+
 pub fn main() {
-    
+    let mut shell = shrust::Shell::new(());
+
+
+    shell.new_command_noargs("hello", "Say 'hello' to the world", |io, _| {
+        writeln!(io, "Hello World !!!")?;
+        Ok(())
+    });
+
+    shell.new_command_noargs("uci", "Continue as a UCI engine", |_,_| { super::uci::main(); Ok(()) });
+    shell.new_command_noargs("perft", "Perform a perft() test", |_,_| { super::perft::main(); Ok(())});
+    shell.new_command_noargs("test", "Runs a predetermined test routine", |_,_| { super::test::main(); Ok(())});
+
+    shell.new_command_noargs("think", "Let the engine think about a move", |_,_| {
+        Ok(())
+    });
+    shell.run_loop(&mut shrust::ShellIO::default());
 }
