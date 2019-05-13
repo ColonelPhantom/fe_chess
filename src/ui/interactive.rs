96ceb58491ae23f653fe25c721eb264763085e38
Quinten Kock <quinten@quinten.space>
2019-05-13 04:15:47 +0200

    Add thinking and state logic to shrust

--- src/ui/interactive.rs
+++ src/ui/interactive.rs
@@ -1,7 +1,20 @@
 use std::io::prelude::*;
+use crate::*;
 
 pub fn main() {
-    let mut shell = shrust::Shell::new(());
+
+    struct State {
+        thought: Option<board::Move>,
+        board: board::Board,
+        depth: usize
+    };
+
+    let s = State {
+        thought: None,
+        board: board::Board::new(),
+        depth: 5,
+    };
+    let mut shell = shrust::Shell::new( s );
 
 
     shell.new_command_noargs("hello", "Say 'hello' to the world", |io, _| {
@@ -13,7 +26,10 @@ pub fn main() {
     shell.new_command_noargs("perft", "Perform a perft() test", |_,_| { super::perft::main(); Ok(())});
     shell.new_command_noargs("test", "Runs a predetermined test routine", |_,_| { super::test::main(); Ok(())});
 
-    shell.new_command_noargs("think", "Let the engine think about a move", |_,_| {
+    shell.new_command_noargs("think", "Let the engine think about a move", |io,s| {
+        s.thought = search::search(&mut s.board, s.depth).pv.pop();
+        let m = s.thought.clone().expect("No move found!");
+        writeln!(io, "Move: {:?}", m)?;
         Ok(())
     });
     shell.run_loop(&mut shrust::ShellIO::default());
