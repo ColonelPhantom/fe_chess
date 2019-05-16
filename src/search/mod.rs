e997057dc341323b5dab3d9317e36bd1161dcbda
Quinten Kock <quintenkock@gmail.com>
2019-05-16 17:28:13 +0200

    Implement an enum Score

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -4,6 +4,88 @@ use board::Board;
 mod alphabeta;
 mod quiesce;
 
+enum Score {
+    Draw,
+    Value(isize),
+    Win(usize),
+    Loss(usize),
+}
+impl std::ops::Neg for Score {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        match self {
+            Score::Draw => Score::Draw,
+            Score::Value(p) => Score::Value(-p),
+            Score::Win(d) => Score::Loss(d),
+            Score::Loss(d) => Score::Win(d),
+        }
+    }
+}
+
+impl std::cmp::PartialOrd for Score {
+    fn partial_cmp(&self, o: &Self) -> Option<std::cmp::Ordering> {
+        Some( self.cmp(o) )
+    }
+}
+
+impl std::cmp::Ord for Score {
+    fn cmp(&self, o: &Self) -> std::cmp::Ordering {
+        use std::cmp::Ordering;
+
+        match self {
+            Score::Draw => match o {
+                Score::Draw => Ordering::Equal,
+                Score::Value(px) => 0.cmp(px),
+                Score::Win(_) => Ordering::Less,
+                Score::Loss(_) => Ordering::Greater,
+            }
+            Score::Value(p) => match o {
+                Score::Draw => p.cmp(&0),
+                Score::Value(px) => p.cmp(px),
+                Score::Win(_) => Ordering::Less,
+                Score::Loss(_) => Ordering::Greater,
+            }
+            Score::Win(d) => match o {
+                Score::Win(dx) => d.cmp(dx),
+                _ => Ordering::Greater,
+            }
+            Score::Loss(d) => match o {
+                Score::Loss(dx) => dx.cmp(d),
+                _ => Ordering::Less,
+            }
+        }
+    }
+}
+
+impl std::cmp::PartialEq for Score {
+    fn eq(&self, o: &Self) -> bool {
+        match self {
+            Score::Draw => match o {
+                Score::Draw => true,
+                Score::Value(p) => *p == 0,
+                _ => false,
+            }
+            Score::Value(p) => match o {
+                Score::Draw => *p == 0,
+                Score::Value(px) => p == px,
+                _ => false
+            }
+            Score::Win(d) => match o {
+                Score::Win(dx) => d == dx,
+                _ => false
+            }
+            Score::Loss(d) => match o {
+                Score::Loss(dx) => d == dx,
+                _ => false
+            }
+        }
+    }
+}
+impl std::cmp::Eq for Score {}
+
+
+
 #[derive(Debug, Clone)]
 pub struct SearchInfo {
     pub score: isize,
