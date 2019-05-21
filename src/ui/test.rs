37a29c823ade77969e979684fad5f1903ced6880
Quinten Kock <quinten@quinten.space>
2019-05-21 02:22:49 +0200

    Make test.rs consider the slowdown per depth

--- src/ui/test.rs
+++ src/ui/test.rs
@@ -2,15 +2,38 @@ use crate::*;
 
 pub fn main() {
     let mut b = board::Board::new();
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
+    let m = &search::search(&mut b, 5, &mut search::transtable::TransTable::new(18)).pv.pop().expect("Did not get a move");
+    b.make(m);
 
-    let mut searchinfo = search::search(&mut b, 8, &mut search::transtable::TransTable::new(24));
-    while let Some(m) = searchinfo.pv.pop() {
-        println!(
-            "{:02x} {:02x} {:?} {:?} {:?}",
-            m.from, m.to, m.promote_to, m.en_passant, m.castling
-        );
-    }
-    println!("Score: {}", searchinfo.score);
+    // let mut searchinfo = search::search(&mut b, 8, &mut search::transtable::TransTable::new(24));
+    // while let Some(m) = searchinfo.pv.pop() {
+    //     println!(
+    //         "{:02x} {:02x} {:?} {:?} {:?}",
+    //         m.from, m.to, m.promote_to, m.en_passant, m.castling
+    //     );
+    // }
+    // println!("Score: {}", searchinfo.score);
+
+    let start = std::time::Instant::now();
+    search::search(&mut b, 4, &mut search::transtable::TransTable::new(24));
+    println!("Time to depth 4: {} ms", start.elapsed().as_millis());
+    let start = std::time::Instant::now();
+    search::search(&mut b, 5, &mut search::transtable::TransTable::new(24));
+    println!("Time to depth 5: {} ms", start.elapsed().as_millis());
+    let start = std::time::Instant::now();
+    search::search(&mut b, 6, &mut search::transtable::TransTable::new(24));
+    println!("Time to depth 6: {} ms", start.elapsed().as_millis());
+    
 
     // println!("Sizeof board: {}", std::mem::size_of::<board::Board>());
     // println!("Sizeof move: {}", std::mem::size_of::<board::Move>());
