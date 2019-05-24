fc3005b2d82f28bc7b4823ab5370ae2981a4f0d4
Quinten Kock <quinten@quinten.space>
2019-05-24 21:51:29 +0200

    Add code for testing to depth 8 in depth

--- src/ui/test.rs
+++ src/ui/test.rs
@@ -35,6 +35,8 @@ pub fn main() {
     measure_time!(search::search(&mut b, 6, &mut tt), "Time to depth 6: {} ms");
     let mut tt = search::transtable::TransTable::new(22);
     measure_time!(search::search(&mut b, 7, &mut tt), "Time to depth 7: {} ms");
+    // let mut tt = search::transtable::TransTable::new(22);
+    // measure_time!(search::search(&mut b, 8, &mut tt), "Time to depth 8: {} ms");
     println!("Transposition table pressure: {}/{}", tt.filled(), 2_u64.pow(22));
     
 
